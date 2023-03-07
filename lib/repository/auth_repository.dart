60f84eecdf77f478c5f5f0e86033ceb996332fbb
Sanskar Modi <modisanskar5@gmail.com>
2023-03-07 19:00:40 +0530

    Auth service with http package added

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,31 +1,82 @@
-import 'dart:developer';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+// This is a Dart file that defines an authentication controller class and some associated providers.
+
+import 'dart:convert';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart';
 
-final authRepositoryProvider = Provider(
-  (ref) => AuthRepository(
-    googleSignIn: GoogleSignIn(),
-  ),
-);
+import '../constants/host_service.dart';
+import '../models/error_model.dart';
+import '../models/user_account.dart';
 
-class AuthRepository {
+// Define a provider that creates an instance of the AuthController class.
+final authRepositoryProvider = Provider((ref) => AuthController(
+      googleSignIn: GoogleSignIn(),
+      client: Client(),
+    ));
+
+// Define a provider that provides a user model.
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
+// Define the AuthController class.
+class AuthController {
   final GoogleSignIn _googleSignIn;
-  AuthRepository({
-    required GoogleSignIn googleSignIn,
-  }) : _googleSignIn = googleSignIn;
+  final Client _client;
 
-  void signInWithGoogle() async {
+  // Define the constructor for the AuthController class.
+  AuthController({
+    required GoogleSignIn googleSignIn,
+    required Client client,
+  })  : _googleSignIn = googleSignIn,
+        _client = client;
+
+  // Define a method that signs in with Google and returns an ErrorModel.
+  Future<ErrorModel> signInWithGoogle() async {
+    // Initialize the error model with a default error message and null data.
+    ErrorModel error =
+        ErrorModel(error: 'Something unexpected happened', data: null);
     try {
+      // Sign in with Google and get the user object.
       final user = await _googleSignIn.signIn();
-      print(user.toString());
       if (user != null) {
-        print(user.email);
-        print(user.displayName!);
-        print(user.photoUrl!);
+        // Create a new UserModel object from the Google user object.
+        final accUser = UserModel(
+          email: user.email,
+          name: user.displayName!,
+          photoUrl: user.photoUrl!,
+          uid: "",
+          token: "",
+        );
+
+        // Send a POST request to the server to sign up the user.
+        var res = await _client.post(Uri.parse("$host/api/signup"),
+            body: accUser.toJson(),
+            headers: {
+              'Content-Type': 'application/json; charset=UTF-8',
+            });
+
+        // Check the response status code.
+        switch (res.statusCode) {
+          // If the status code is 200, create a new UserModel object with the user ID
+          // returned by the server and update the error model with the new user data.
+          case 200:
+            final newUSer = accUser.copyWith(
+              uid: jsonDecode(res.body)['user']['_id'],
+            );
+            error = ErrorModel(error: null, data: newUSer);
+            break;
+          // If the status code is not 200, print an error message to the console.
+          default:
+            print('error');
+        }
       }
     } catch (e) {
-      log("Error = ${e.toString()}");
+      // If an exception occurs, update the error model with the exception message.
+      error = ErrorModel(error: e.toString(), data: null);
     }
+    // Return the error model.
+    return error;
   }
 }
