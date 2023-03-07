e484fbb7cd5c5cdacbb41a38ee389d7b528ec7ad
Sanskar Modi <modisanskar5@gmail.com>
2023-03-07 19:00:07 +0530

    User Model , Error Model and Home Screen added

---
+++ lib/models/user_account.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserModel {
+  final String name;
+  final String email;
+  final String photoUrl;
+  final String uid;
+  final String token;
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.photoUrl,
+    required this.uid,
+    required this.token,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'email': email,
+      'photoUrl': photoUrl,
+      '_id': uid,
+      'token': token,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'] as String,
+      email: map['email'] as String,
+      photoUrl: map['photoUrl'] as String,
+      uid: map['_id'] as String,
+      token: map['token'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  UserModel copyWith({
+    String? name,
+    String? email,
+    String? photoUrl,
+    String? uid,
+    String? token,
+  }) {
+    return UserModel(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      photoUrl: photoUrl ?? this.photoUrl,
+      uid: uid ?? this.uid,
+      token: token ?? this.token,
+    );
+  }
+}
---
+++ lib/ui/home_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../repository/auth_repository.dart';
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(child: Text(ref.watch(userProvider)!.name)),
+    );
+  }
+}
