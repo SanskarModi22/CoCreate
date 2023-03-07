3e5766b86698b556229f6bff8556f290fbb474ef
Sanskar Modi <modisanskar5@gmail.com>
2023-03-07 12:43:32 +0530

    Mongoose Connection setup

--- server/index.js
+++ server/index.js
@@ -5,13 +5,12 @@ const PORT = process.env.PORT | 8000;
 const app = express();
 console.log(process.env.DATABASE);
 mongoose
-    .connect(process.env.DATABASE)
-    .then(() => {
-        console.log("DB Connected");
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
-    .catch((err) => {
-        console.log(err);
-    });
+    .then(() => console.log("DB connected"))
+    .catch((e) => console.log(e));
 app.listen(PORT, "0.0.0.0", () => {
     console.log("Server is running on port 8000");
 });
